7043c9df07bc423ab39571a293c0767b591955c9
Sanjay <sanjay@esigners.in>
2024-08-13 12:07:51 +0530

    Updates

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# php-mailer
+# Email using PHP Mailer
+
+## Installation
+
+1. Log into your server via SSH.
+2. Make sure you're in your user's home directory.
+3. Download the zip file from GitHub.com. [server]$ wget https://github.com/PHPMailer/PHPMailer/archive/master.zip
+4. Unzip the file. [server]$ unzip master.zip
+5. This creates a directory named PHPMailer-master.
+6. Rename this directory. [server]$ mv PHPMailer-master PHPMailer
+7. Create a new PHP file and name it something like phpemail.php
+8. It can be named anything and be placed in any directory.
+9. Insert the following code into the PHP file.
+10. Modify the details as required
  
---
+++ phpemail.php
@@ -0,0 +1,47 @@
+<?php
+// Import PHPMailer classes into the global namespace
+// These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '/home/username/PHPMailer/src/Exception.php';
+require '/home/username/PHPMailer/src/PHPMailer.php';
+require '/home/username/PHPMailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+try {
+    //Server settings
+    $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.domainname.com';                  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'contact@example.com';             // SMTP username
+    $mail->Password = 'secret';                           // SMTP password
+    $mail->SMTPSecure = 'ssl';                            // Enable SSL encryption, TLS also accepted with port 465
+    $mail->Port = 465;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('contact@example.com', 'Mailer');          //This is the email your form sends From
+    $mail->addAddress('recipient@example.com', 'Joe User'); // Add a recipient address
+    //$mail->addAddress('contact@example.com');               // Name is optional
+    //$mail->addReplyTo('info@example.com', 'Information');
+    //$mail->addCC('cc@example.com');
+    //$mail->addBCC('bcc@example.com');
+
+    //Attachments
+    //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+    //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Subject line goes here';
+    $mail->Body    = 'Body text goes here';
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
+?>
